9d9f2d14b76b7f2c819d51a45ff4181b5d53f355
kobayashi-m42 <m42kobayashi@gmail.com>
2019-11-02 13:51:40 +0900

    :sparkles: #3 既存のコンポーネントのStoryファイルを作成

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../App';
+
+export default {
+  title: 'App',
+};
+
+export const showApp = () => <App />;
+
+showApp.story = {
+  name: 'show app',
+};
