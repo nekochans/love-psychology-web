3ff90b8f383ea67e21e9709c542200a29750f7ec
kobayashi-m42 <m42kobayashi@gmail.com>
2019-11-02 14:13:45 +0900

    :heavy_plus_sign: #3 storybookのaddonを追加

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withA11y } from '@storybook/addon-a11y';
 
 addDecorator(withInfo);
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.tsx$/), module);
